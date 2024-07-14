2e05c59b5dae6352517a96e11affe5b4fd284172
PepcyCh <pepsich86@163.com>
2024-07-14 18:15:52 +0800

    xmake: Update xmake a little.

--- bisemutum/xmake.lua
+++ bisemutum/xmake.lua
@@ -1,10 +1,18 @@
 add_requires("fmt", "spdlog", "miniz", "glm", "entt", "magic_enum")
 add_requires("glfw", "directxshadercompiler", "crypto-algorithms")
-add_requires("vulkan-headers", "vulkan-memory-allocator")
 add_requires("nlohmann_json", "toml++", "assimp", "tinygltf", "mikktspace", "stb", "tinyexr")
 add_requires("imgui v1.89.9-docking", {configs = {glfw = true}})
+
+graphics_backend_vulkan = false
+graphics_backend_d3d12 = false
+
+if is_plat("windows") or is_plat("linux") then
+    add_requires("vulkan-headers", "vulkan-memory-allocator")
+    graphics_backend_vulkan = true
+end
 if is_plat("windows") then
     add_requires("d3d12-memory-allocator")
+    graphics_backend_d3d12 = true
 end
 
 includes("thirdparty/xmake.lua")
@@ -23,18 +31,30 @@ target("bisemutum-lib")
     add_packages("fmt", "spdlog", "miniz", "glm", "entt", "magic_enum", "imgui", {public = true})
     add_defines("MAGIC_ENUM_RANGE_MAX=8192")
     add_packages("glfw", "directxshadercompiler", "crypto-algorithms")
-    add_packages("vulkan-headers", "vulkan-memory-allocator")
-    add_defines("VK_NO_PROTOTYPES")
     add_packages("nlohmann_json", "toml++", "assimp", "tinygltf", "mikktspace", "stb", "tinyexr")
-    if is_plat("windows") then
-        add_defines("VK_USE_PLATFORM_WIN32_KHR", "NOMINMAX")
+
+    if graphics_backend_vulkan then
+        add_defines("BI_GRAPHICS_BACKEND_VULKAN")
+        add_defines("VK_NO_PROTOTYPES")
+        add_packages("vulkan-headers", "vulkan-memory-allocator")
+    else
+        remove_files("src/rhi/backend_vulkan/**.cpp")
+        remove_headerfiles("src/rhi/backend_vulkan/**.hpp")
+    end
+    if graphics_backend_d3d12 then
+        add_defines("BI_GRAPHICS_BACKEND_D3D12")
         add_packages("d3d12-memory-allocator")
         add_syslinks("d3d12", "dxgi", "dxguid")
-        add_deps("winpix")
     else
         remove_files("src/rhi/backend_d3d12/**.cpp")
         remove_headerfiles("src/rhi/backend_d3d12/**.hpp")
     end
+
+    if is_plat("windows") then 
+        add_defines("VK_USE_PLATFORM_WIN32_KHR", "NOMINMAX")
+        add_deps("winpix")
+    end
+
     after_build(function (target) 
         io.writefile(target:targetdir().."/bisemutum_path.txt", target:scriptdir())
     end)
